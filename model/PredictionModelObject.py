ebddfacc34f426b991257fad3fbee2c743df6418
Matías Saavedra <m.saavedra06@ufromail.cl>
2021-11-24 03:37:49 -0300

    WIP estructura básica de proyecto.

---
+++ model/PredictionModelObject.py
@@ -0,0 +1,19 @@
+from fastai.basic_train import load_learner
+from fastai.vision import open_image
+from flask_cors import CORS
+
+class PredictionModelObject:
+    def __init__(self):
+        self.model="model-roadsign.pkl"
+        self.learn = load_learner(path='../prediction_model', file=self.model)
+        self.classes = self.learn.data.classes
+
+
+    def predict_single(self,img_file):
+        'function to take image and return prediction'
+        prediction = self.learn.predict(open_image(img_file))
+        probs_list = prediction[2].numpy()
+        return {
+            'category': self.classes[prediction[1].item()],
+            'probs': {c: round(float(probs_list[i]), 5) for (i, c) in enumerate(self.classes)}
+        }    
